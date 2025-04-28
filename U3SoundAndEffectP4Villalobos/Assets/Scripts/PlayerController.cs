51f7eab28c2a4cdde113212e15a70b6f94746604
doonska <727187@student.dusd.net>
2025-04-28 11:58:44 -0700

    ewrfrwef
    reer

--- U3SoundAndEffectP4Villalobos/Assets/Scripts/PlayerController.cs
+++ U3SoundAndEffectP4Villalobos/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,7 +30,11 @@ public class PlayerController : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            isOnGround = true;
+            Debug.Log("Game Over");
+        }
     }
 
 }
