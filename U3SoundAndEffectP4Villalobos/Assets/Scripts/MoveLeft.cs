e13f5d87d373c5af0be637f53b25baaeed81557d
doonska <727187@student.dusd.net>
2025-04-30 11:46:03 -0700

    U3
    started animations on step 4

--- U3SoundAndEffectP4Villalobos/Assets/Scripts/MoveLeft.cs
+++ U3SoundAndEffectP4Villalobos/Assets/Scripts/MoveLeft.cs
@@ -6,6 +6,7 @@ public class MoveLeft : MonoBehaviour
 {
     private float speed = 20;
     private PlayerController playerControllerScript;
+    private float leftBound = -15;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +17,14 @@ public class MoveLeft : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+      if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+           
+        }
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
